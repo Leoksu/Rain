88bb83aeb8b7d03bfa4c93835322e0fbb33bc63a
eleven <leoksuck@gmail.com>
2023-01-28 20:03:29 +0700

    update stuff/config.py

--- stuff/config.py
+++ stuff/config.py
@@ -1,7 +1,7 @@
 from decouple import config
 
-AI_API_KEY = config("AI_API_KEY", default=None)
-AI_BID = config("AI_BID", default=None, cast=int)
+AI_API_KEY = config("API_URL", default=None)
+AI_BID = config("BRAIN_ID", default=None, cast=int)
 API_ID = config("API_ID", default=6)
 API_HASH = config("API_HASH", default="eb06d4abfb49dc3eeb1aeb98ae0f581e")
 bot_token = config("BOT_TOKEN", default=None)
